53106838dfe8d97592719e13e84c84bd539616fa
AFKFXH <AFKFXH@SZA0030606>
2023-11-14 10:27:39 +0100

    Add project files.

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,28 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseDefaultFiles(); // alap mapping, pl. ha siám az oldal címére megyünk is a index.html-re dobjon
+app.UseStaticFiles(); // statikus html fájlok engedélyezése
+
+app.MapControllers();
+
+app.Run();
